75311f1a2f0ce3b1795b918826bbca380d037b4b
yazninja <yazlesean@gmail.com>
2022-10-01 15:13:59 +0800

    Proposal UI Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,44 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
+  <q-layout view="hHh Lpr lFf">
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -47,7 +8,6 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
@@ -96,10 +56,6 @@ const linksList = [
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
 
   setup () {
     const leftDrawerOpen = ref(false)
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,52 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center column">
+    <div class="q-gutter-md row items-start">
+      <q-file
+        v-model="files"
+        label="Input Excel File"
+        filled
+        counter
+        :counter-label="counterLabelFn"
+        accept=".xlsx"
+        style="width: 400px;"
+      >
+        <template v-slot:prepend>
+          <q-icon name="attach_file" />
+        </template>
+      </q-file>
+    </div>
+    <div v-if="files" class="q-gutter-sm">
+      <q-btn push rounded color="primary" label="Compile Data" @click="dialogCompiled=true"/>
+      <q-btn push rounded color="secondary" label="Check For Errors" @click="dialogErrors=true"/>
+    </div>
+    <q-dialog v-model="dialogCompiled" position="bottom">
+      <q-card class="flex flex-center q-gutter-md" style="width: 350px">
+        <div class="text-bold">Data Compiled</div>
+      </q-card>
+    </q-dialog>
+    <q-dialog v-model="dialogErrors" position="bottom">
+      <q-card class="flex flex-center q-gutter-md" style="width: 350px">
+        <div class="text-bold">No Errors Found</div>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup() {
+    return {
+      dialogCompiled : ref(false),
+      dialogErrors : ref(false),
+      files: ref(null),
+      counterLabelFn ({ totalSize, filesNumber, maxFiles }) {
+        return filesNumber < 1 ? "" : totalSize
+      }
+    }
+    
+  }
 })
 </script>
