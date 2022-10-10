f4523835ef63c27b19e3b089e78eca451d17548a
patmnic <patricia_nicole_martinez@dlsu.edu.ph>
2022-10-10 09:39:44 +0800

    componentize ui (unfinished)

---
+++ src/components/ActionButtons.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-center" style="gap: 10px">
+    <q-btn
+      push
+      rounded
+      color="primaryColor"
+      label="primaryLabel"
+      @click="dialogCompiled = true && compileData()"
+    />
+    <q-btn
+      push
+      rounded
+      color="secondaryColor"
+      label="secondaryLabel"
+      @click="dialogErrors = true"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'FileInput',
+  props: {
+    primaryColor: {
+      type: String,
+      default: 'primary'
+    },
+    secondaryColor: {
+      type: String,
+      default: 'secondary'
+    },
+    primaryLabel: {
+      type: String,
+      default: 'Compile Data'
+    },
+    secondaryLabel: {
+      type: String,
+      default: 'Check For Errors'
+    }
+  }
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,20 +1,40 @@
 <template>
     <q-page class="flex flex-center column">
         <FileInput placeholder="Input Excel File" :model="files"></FileInput>
+        <ActionButtons> </ActionButtons>
+        <DialogueComp placeholder="Data Compiled" :model="dialogCompiled"></DialogueComp>
     </q-page>
 </template>
 <script>
 import { defineComponent, ref } from "vue";
 import FileInput from "../components/FileInput.vue";
+import QuickEdit from "../components/QuickEdit.vue";
+import ActionButtons from "../components/ActionButtons.vue"
+import DialogueComp from "../components/DialogueComp.vue";
 export default defineComponent({
-    name: "IndexPage",
-    components: {
-        FileInput
-    },
-    setup() {
-        return {
-            files: ref(null),
-        }
-    },
+  name: "IndexPage",
+  components: {
+    FileInput,
+    //QuickEdit,
+    ActionButtons,
+    DialogueComp
+  },
+  setup() {
+    return {
+      dialogCompiled: ref(false),
+      dialogErrors: ref(false),
+      files: ref(null),
+      errorMessage: ref(null),
+      async compileData() {
+        this.busy = true;
+        setTimeout(() => {
+          this.busy = false;
+        }, 1000);
+        console.log("Compiling Data");
+        this.errorMessage = "Lorem Ispum Dolor Sit Amet";
+        this.showError = true;
+      }
+    }
+  }
 });
 </script>
