d0af8559a0b2a21e71b2dcd081f5f9b9b550c58e
Kevin Abelgas <kev.abelgas@gmail.com>
2022-10-10 18:00:53 +0800

    loadXlsx semi-working

--- src-electron/electron-main.js
+++ src-electron/electron-main.js
@@ -5,7 +5,7 @@ import { readFile } from 'fs/promises'
 import ExcelJS from 'exceljs'
 import os from 'os'
 import { event } from 'quasar';
-import { default as summarizeData }from './handlers/summarizeData.js'
+import { default as summarizeData } from './handlers/summarizeData.js'
 import { loadXlsx } from './handlers/loadXlsx.js'
 
 // needed in case process is undefined under Linux
@@ -19,7 +19,7 @@ try {
 
 let mainWindow
 
-if(os.release().split('.')[2] >= 22000) {
+if (os.release().split('.')[2] >= 22000) {
   app.commandLine.appendSwitch("enable-transparent-visuals");
 }
 
@@ -27,8 +27,8 @@ function createWindow() {
   /**
    * Initial window options
    */
-  if(os.release().split('.')[2] >= 22000) {
-  mainWindow = new MicaBrowserWindow({
+  if (os.release().split('.')[2] >= 22000) {
+    mainWindow = new MicaBrowserWindow({
       icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
       width: 1000,
       height: 600,
@@ -82,7 +82,7 @@ function createWindow() {
     mainWindow.webContents.send('theme-changed', nativeTheme.shouldUseDarkColors)
   })
 
-  
+
 
 }
 
@@ -108,73 +108,7 @@ ipcMain.handle('isMica', async (event, arg) => {
 })
 summarizeData(); // basically just call the funtion/s
 
-loadXlsx(); //
-
-/*
-ipcMain.handle('loadXlsx', async (event, path) => {
-  let excelFile = await readFile(path).catch(err => console.log(err))
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.load(excelFile);
-
-  let players = [];
-  // Initialize Players
-  let playWorksheet = workbook.getWorksheet("Jojo Bettors")
-  if(!playWorksheet) { return { error: "Missing Jojo Bettors Worksheet", errorType: "error"} }
-  playWorksheet.eachRow(row => {
-    if (row._cells[0].value === null) return
-    players.push({
-      name: row._cells[0].value,
-      tong: row._cells[1].value,
-      comm: row._cells[2].value,
-      bets: []
-    })
-  })
-
-  let dayWorksheets = [];
-  let daysRegex = /\b((mon|tue|wed(nes)?|thu(rs)?|fri|sat(ur)?|sun)(day)?)\b/gi
-  workbook.eachSheet((sheet, id) => {
-    if (sheet.name.match(daysRegex)) dayWorksheets.push(workbook.getWorksheet(id))
-  })
-
-  dayWorksheets.forEach(sheet => {
-    let playerRow = sheet.getRow(1).values;
-    playerRow = playerRow.filter((value, index) => {
-      if (value.formula?.includes("Jojo Bettors")) {
-        value.index = index
-        return value
-      }
-    })
-    playerRow.forEach(p => {
-      players.forEach(player => {
-        if (p.result === player.name) {
-          let column = sheet.getColumn(p.index)
-          column.eachCell((cell, rowNum) => {
-            if (typeof (cell.value) == "number") {
-              let bet = {};
-              bet.day = sheet.name
-              bet.amount = cell.value;
-              // console.log(sheet.getRow(rowNum).getCell(1).value)
-              if (sheet.getRow(rowNum).getCell(1).value.match(/under/gi)) {
-                bet.team = `${sheet.getRow(rowNum - 2).getCell(1).value} / ${sheet.getRow(rowNum).getCell(1).value.toUpperCase()}`
-              }
-              else if (sheet.getRow(rowNum).getCell(1).value.match(/over/gi)) {
-                bet.team = `${sheet.getRow(rowNum - 3).getCell(1).value} / ${sheet.getRow(rowNum).getCell(1).value.toUpperCase()}`
-              }
-              else {
-                bet.team = sheet.getRow(rowNum).getCell(1).value
-              }
-              bet.result = sheet.getRow(rowNum).getCell(3).value
-              player.bets.push(bet)
-            }
-          })
-        }
-      })
-    })
-  })
-  console.log(`Loaded ${players.length} players`)
-  return players
-})
-*/
+loadXlsx(path);
 
 // ipcMain.handle('summarizeData', async (event, data) => {
 //   data.forEach(sheet => {
--- src-electron/handlers/loadXlsx.js
+++ src-electron/handlers/loadXlsx.js
@@ -1,11 +1,104 @@
-import { IpcMain } from "electron";
+import { ipcMain } from "electron";
 import { readFile } from "fs/promises";
 import ExcelJS from "exceljs";
-export async function loadXlsx(path) {
-    let excelFile = await readFile(path).catch(err => console.log(err))
-    const workbook = new ExcelJS.Workbook();
-    await workbook.xlsx.load(excelFile);
 
-    loadPlayers();
-    loadDays();
+// Initialize Players
+async function loadPlayers(workbook) {
+  let players = [];
+  let playWorksheet = workbook.getWorksheet("Jojo Bettors")
+
+  // Search each row for data in non-empty cells and add into players
+  playWorksheet.eachRow(row => {
+    if (row._cells[0].value === null) return
+    players.push({
+      name: row._cells[0].value,
+      tong: row._cells[1].value,
+      comm: row._cells[2].value,
+      bets: []
+    })
+  })
+
+  return loadPlayerData(players)
 }
+
+//Loading player bet data
+async function loadPlayerData(players) {
+
+  let dayWorksheets = await loadDays(wb)
+
+  dayWorksheets.forEach(sheet => {
+    //Get 1st row headers and get only names found in Jojo Bettors sheet (formula uses data from Jojo Bettors)
+    let playerRow = sheet.getRow(1).values;
+    playerRow = playerRow.filter((value, index) => {
+      if (value.formula?.includes("Jojo Bettors")) {
+        value.index = index
+        return value
+      }
+    })
+
+    // Copy every players bets into players.bets
+    playerRow.forEach(p => {
+      players.forEach(player => {
+        // Matching data from playerRow and players
+        if (p.result === player.name) {
+          //Access specific columns belonging to each player based on their index
+          let column = sheet.getColumn(p.index)
+          column.eachCell((cell, rowNum) => {
+            if (typeof (cell.value) == "number") {
+              let bet = {};
+              bet.day = sheet.name
+              bet.amount = cell.value;
+              if (sheet.getRow(rowNum).getCell(1).value === 'UNDER') {
+                bet.teamExtra = sheet.getRow(rowNum).getCell(1).value
+                bet.team = sheet.getRow(rowNum - 2).getCell(1).value
+              }
+              else if (sheet.getRow(rowNum).getCell(1).value === "OVER") {
+                bet.teamExtra = sheet.getRow(rowNum).getCell(1).value
+                bet.team = sheet.getRow(rowNum - 3).getCell(1).value
+              }
+              else {
+                bet.team = sheet.getRow(rowNum).getCell(1).value
+              }
+              bet.result = sheet.getRow(rowNum).getCell(3).value
+              player.bets.push(bet)
+            }
+          })
+        }
+      })
+    })
+  })
+  return players
+}
+
+// Initialize days
+async function loadDays(workbook) {
+  // load player data from specific days
+  let dayWorksheets = [];
+  let daysRegex = /\b((mon|tue|wed(nes)?|thu(rs)?|fri|sat(ur)?|sun)(day)?)\b/gi
+
+  // Search and add days to dayWorksheets
+  // TODO: remove undefined element at the end of dayWorksheets array
+  workbook.eachSheet((sheet, id) => {
+    if ((sheet.name !== undefined) && sheet.name.match(daysRegex)) dayWorksheets.push(workbook.getWorksheet(id))
+  })
+
+  //console.log(dayWorksheets.forEach((s, i) => {console.log(s.name + i)}))
+  return dayWorksheets
+}
+
+
+export async function loadXlsx(path) {
+  ipcMain.handle('loadXlsx', async (event, path) => {
+    let excelFile = await readFile(path).catch(err => console.log(err))
+    //const wb = new ExcelJS.Workbook();
+    await wb.xlsx.load(excelFile);
+
+    let players = await loadPlayers(wb)
+
+    console.log(`Loaded ${players.length} players`)
+    console.log(players[0].bets)
+    return players
+  })
+}
+
+const wb = new ExcelJS.Workbook()
