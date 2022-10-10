4713a074ab4465ca21937d34ecf55b14ce735813
yazninja <yazlesean@gmail.com>
2022-10-10 20:38:45 +0800

    fix action buttons and quickview

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,18 @@
 <template>
     <q-page class="flex flex-center column" :class="!isMica && darkMode && 'dark'">
-        <FileInput placeholder="Input Excel File" :model="files" :players="players" :dark="darkMode"></FileInput>
-        <div v-if="files && (!players || busy)">
-            <q-spinner-cube color="primary" size="2em" />
+        <FileInput placeholder="Input Excel File" :dark="darkMode" :busy="busy" @changeFile="parseXlsx($event)"></FileInput>
+        <div v-if="file && (!players || busy)">
+            <q-spinner-cube color="green" size="2em" />
         </div>
-        <div v-if="players && !busy" class="q-gutter-sm">
-            <QuickEdit></QuickEdit>
+        <div v-if="players && !busy" class="actionDiv q-gutter-sm">
+            <QuickEdit :dark="darkMode" :players="players"></QuickEdit>
+            <div class="flex flex-center" style="gap: 10px">
+                <ActionButton color="primary" label="Data Compile" @click="handleButtonClick($event)"/>
+                <ActionButton color="secondary" label="Check For Errors" @click="handleButtonClick($event)"/>
+            </div>
+            
         </div>
-        <ActionButtons> </ActionButtons>
+        
         <DialogueComp placeholder="Data Compiled" :model="dialogCompiled"></DialogueComp>
     </q-page>
 </template>
@@ -15,6 +20,9 @@
 <style scoped>
     .q-page.dark {
         background-color: #1d1d1d;
+    }
+    .actionDiv {
+        width: 400px;
     }
 </style>
 
@@ -24,14 +32,14 @@ import { useQuasar } from 'quasar'
 
 import FileInput from "../components/FileInput.vue";
 import QuickEdit from "../components/QuickEdit.vue";
-import ActionButtons from "../components/ActionButtons.vue"
+import ActionButton from "../components/ActionButtons.vue"
 import DialogueComp from "../components/DialogueComp.vue";
 export default defineComponent({
     name: "IndexPage",
     components: {
         FileInput,
         QuickEdit,
-        ActionButtons,
+        ActionButton,
         DialogueComp
     },
     setup() {
@@ -43,25 +51,63 @@ export default defineComponent({
         return {
             dialogCompiled: ref(false),
             dialogErrors: ref(false),
-            files: ref(null),
+            file: ref(null),
             players: ref(null),
+            busy: ref(false),
             errorMessage: ref(null),
             darkMode: ref(false),
             isMica: ref(false),
             showError: ref(false),
+
+            handleError() {
+                let message = this.players.error;
+                console.log(`Error: ${message}`);
+                this.file = null;
+                this.players = null;
+                this.errorMessage = null;
+                this.showError = false;
+                this.busy = false;
+
+                
+            },
+            handleWarning() {
+                let message = this.players.warning;
+                console.log(`Warning: ${message}`);
+                this.busy = false;
+            },
+            async parseXlsx(f) {
+                this.file = f;
+                this.busy=true;
+                if(!f) return this.clearVariables();
+                this.players = await window.ipcRenderer.invoke("loadXlsx", f.path);
+                if(this.players?.error ) return this.clearVariables();
+                else if(this.players?.warning) {
+                    this.errorMessage = this.players.error;
+                    this.errorType = this.players.errorType;
+                    this.showError = true;
+                }
+                this.busy = false;
+            },
+            async handleButtonClick(e) {
+                if(e == "Data Compile") {
+                    this.busy = true;
+                    await window.ipcRenderer.invoke("compileData", this.players);
+                    this.busy = false;
+                    this.dialogCompiled = true;
+                }
+                else if(e == "Check For Errors") {
+                    this.busy = true;
+                    await window.ipcRenderer.invoke("checkForErrors", this.players);
+                    this.busy = false;
+                    this.dialogErrors = true;
+                }
+            }
         }
     },
     async mounted() {
-        await window.ipcRenderer.getThemeMode().then((res) => {
-            this.darkMode = res;
-        });
-        window.ipcRenderer.receive("theme-changed", async (arg) => {
-            this.darkMode = arg;
-        });
-        window.ipcRenderer.invoke("isMica").then( async (arg) => {
-            this.isMica = arg;
-        });
-
+        await window.ipcRenderer.getThemeMode().then((res) => this.darkMode = res);
+        window.ipcRenderer.receive("theme-changed", async (arg) => this.darkMode = arg);
+        window.ipcRenderer.invoke("isMica").then( async (arg) => this.isMica = arg);
     },
 });
 </script>
